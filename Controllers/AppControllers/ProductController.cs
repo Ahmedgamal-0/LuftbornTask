25b7ae6413e85453ca2caf240ca8e582ab49b4e3
Ahmed Gamal <a.gamal93455@gmail.com>
2024-11-16 04:39:53 +0200

    Add project files.

---
+++ Controllers/AppControllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Luftborn.Application.Featuers.Product.Command.Models;
+using LuftbornTask.Controllers.BaseControllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuftbornTask.Controllers.AppControllers
+{
+    public class ProductController:BaseController
+    {
+        #region Vars / Props
+        #endregion
+
+        #region Constructor(s)
+        public ProductController()
+        {
+        }
+        #endregion
+
+        #region Actions
+
+        [HttpPost]
+        //[Authorize(Policy = Permissions.BookingEngine.Product.Write)]
+        public async Task<IActionResult> Register(RegisterProductCommand command)
+        {
+            var mediatorResponse = await Mediator.Send(command);
+            //if (mediatorResponse.Succeeded)
+            //{
+            //    await _logActivityService.InsertAsync(LastUpdatedBy, LoggingConstants.BookingEngine.ADD_NEW_Product, mediatorResponse.EntityId, "used command", "Booking Number Select Step", "", "Booking Engine", command);
+            //}
+
+            return Ok(mediatorResponse);
+        }
+        #endregion
+    }
+}
---
+++ Controllers/BaseControllers/BaseController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace LuftbornTask.Controllers.BaseControllers
+{
+    [ApiController]
+    [Route(BasePath + "/[controller]")]
+    [ApiVersion("1.0")]
+    public abstract class BaseController : ControllerBase
+    {
+        #region Vars / Props
+        protected internal const string BasePath = "api/luftborn";
+        private IMediator _mediatorInstance;
+        protected string LastUpdatedBy;
+        protected IMediator Mediator
+        {
+            get
+            {
+
+                var mediator = _mediatorInstance ??= HttpContext.RequestServices.GetService<IMediator>();
+                InitiateOnMediatorRequest();
+                return mediator;
+            }
+        }
+        protected IMediator UnAuthorizedMediator
+        {
+            get
+            {
+
+                var mediator = _mediatorInstance ??= HttpContext.RequestServices.GetService<IMediator>();
+                return mediator;
+            }
+        }
+        #endregion
+
+        #region Constructor(s)
+        public BaseController()
+        {
+
+        }
+
+        #endregion
+
+        #region Actions
+        void InitiateOnMediatorRequest()
+        {
+            //Microsoft.Extensions.Primitives.StringValues token;
+            //Request.Headers.TryGetValue("Authorization", out token);
+            //LastUpdatedBy = JwtTokenHelper.ExtractUserIdFromToken(token);
+        }
+        #endregion
+    }
+}
